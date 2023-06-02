cb3f51b29f2a3499d1ce64a00418e1bada686bf9
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-06-02 15:54:50 +0700

    n+1 problem fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,8 +28,8 @@ class DatabaseSeeder extends Seeder
         );
         User::create(
             [
-                'name' => 'Arif Budiman Arrosyid',
-                'email' => 'arifbudimanarrosyid@gmail.com',
+                'name' => 'Tiara Sekarsini',
+                'email' => 'tiara.sekarsini.ft20@mail.umy.ac.id',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
---
+++ tugas9.md
@@ -0,0 +1,13 @@
+# Tugas 7
+
+## 1. Browser Todo
+
+![Alt text](screenshot/tugas9/BrowserTodo.png)
+
+## 2. Browser Category
+
+![Alt text](screenshot/tugas9/BrowserCategory.png)
+
+## 2. Browser User
+
+![Alt text](screenshot/tugas9/BrowserUser.png)
